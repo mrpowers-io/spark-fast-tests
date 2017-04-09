ab3674622792afeb656d653ed8d2eaa0a9091496
MrPowers <matthewkevinpowers@gmail.com>
2017-04-09 17:01:55 -0500

    Add an RDDComparer trait to compare two RDDs

---
+++ src/main/scala/com/github/mrpowers/spark/fast/tests/RDDComparer.scala
@@ -0,0 +1,26 @@
+package com.github.mrpowers.spark.fast.tests
+
+import org.apache.spark.rdd.RDD
+
+import scala.reflect.ClassTag
+
+case class RDDContentMismatch(smth: String) extends Exception(smth)
+
+trait RDDComparer {
+
+  def contentMismatchMessage[T: ClassTag](actualRDD: RDD[T], expectedRDD: RDD[T]): String = {
+    s"""
+Actual RDD Content:
+${actualRDD.take(5).mkString("\n")}
+Expected RDD Content:
+${expectedRDD.take(5).mkString("\n")}
+"""
+  }
+
+  def assertSmallRDDEquality[T: ClassTag](actualRDD: RDD[T], expectedRDD: RDD[T]): Unit = {
+    if (!actualRDD.collect().sameElements(expectedRDD.collect())) {
+      throw new RDDContentMismatch(contentMismatchMessage(actualRDD, expectedRDD))
+    }
+  }
+
+}
