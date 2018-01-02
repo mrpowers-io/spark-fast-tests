dc294aff4fc80d637fb1a132c773932224f9c9b6
MrPowers <matthewkevinpowers@gmail.com>
2018-01-02 07:15:31 -0500

    Refactor the assertColumnEquality method to provide a better error message and hopefully make the code a bit faster

--- src/main/scala/com/github/mrpowers/spark/fast/tests/ColumnComparer.scala
+++ src/main/scala/com/github/mrpowers/spark/fast/tests/ColumnComparer.scala
@@ -1,6 +1,6 @@
 package com.github.mrpowers.spark.fast.tests
 
-import org.apache.spark.sql.{Column, DataFrame}
+import org.apache.spark.sql.DataFrame
 
 case class ColumnMismatch(smth: String) extends Exception(smth)
 
@@ -11,14 +11,15 @@ trait ColumnComparer {
     colName1: String,
     colName2: String
   ): Unit = {
-    val colName1Elements = df.select(colName1).collect()
-    val colName2Elements = df.select(colName2).collect()
+    val elements = df.select(colName1, colName2).collect()
+    val colName1Elements = elements.map(_(0))
+    val colName2Elements = elements.map(_(1))
     val mismatchMessage = s"""
 Columns aren't equal
-colName1 Elements:
-${colName1Elements.mkString(", ")}
-colName2 Elements:
-${colName2Elements.mkString(", ")}
+'$colName1' elements:
+[${colName1Elements.mkString(", ")}]
+'$colName2' elements:
+[${colName2Elements.mkString(", ")}]
 """
     if (!colName1Elements.sameElements(colName2Elements)) {
       throw ColumnMismatch(mismatchMessage)
