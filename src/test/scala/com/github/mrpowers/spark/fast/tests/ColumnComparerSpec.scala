1bccbb50b453f20efed314ec8486fb10a4cadf2b
MrPowers <matthewkevinpowers@gmail.com>
2018-01-15 10:00:23 -0500

    Demonstrate how to use assertColumnEquality for cells that contain an array with a single empty string

--- src/test/scala/com/github/mrpowers/spark/fast/tests/ColumnComparerSpec.scala
+++ src/test/scala/com/github/mrpowers/spark/fast/tests/ColumnComparerSpec.scala
@@ -1,6 +1,7 @@
 package com.github.mrpowers.spark.fast.tests
 
 import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.scalatest.FunSpec
 
@@ -125,6 +126,44 @@ class ColumnComparerSpec
       )
 
       assertColumnEquality(sourceDF, "l1", "l2")
+
+    }
+
+    it("works for computed ArrayType columns") {
+
+      val sourceData = Seq(
+        Row("i like blue and red", Array("blue", "red")),
+        Row("you pink and blue", Array("blue", "pink")),
+        Row("i like fun", Array(""))
+      )
+
+      val sourceSchema = List(
+        StructField("words", StringType, true),
+        StructField("expected_colors", ArrayType(StringType, true), true)
+      )
+
+      val sourceDF = spark.createDataFrame(
+        spark.sparkContext.parallelize(sourceData),
+        StructType(sourceSchema)
+      )
+
+      val actualDF = sourceDF.withColumn(
+        "colors",
+        split(
+          concat_ws(
+            ",",
+            when(col("words").contains("blue"), "blue"),
+            when(col("words").contains("red"), "red"),
+            when(col("words").contains("pink"), "pink"),
+            when(col("words").contains("cyan"), "cyan")
+          ),
+          ","
+        )
+      )
+
+      //      actualDF.show(truncate = false)
+
+      assertColumnEquality(actualDF, "colors", "expected_colors")
 
     }
 
