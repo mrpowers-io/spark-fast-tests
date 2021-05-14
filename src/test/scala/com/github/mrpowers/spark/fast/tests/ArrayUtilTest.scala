93474d300d0d19cd051aa95f80c1082a8fd0d405
Stephen Kestle <stephen@kairos.offline.internal>
2021-05-14 14:04:51 +1200

    Added `Array.deep` method to allow compatibility with 2.13
    Merged ArrayPrettyPrint with deep into ArrayUtil

--- src/test/scala/com/github/mrpowers/spark/fast/tests/ArrayUtilTest.scala
+++ src/test/scala/com/github/mrpowers/spark/fast/tests/ArrayUtilTest.scala
@@ -4,30 +4,30 @@ import java.sql.Date
 import java.time.LocalDate
 import org.scalatest.FreeSpec
 
-class ArrayPrettyPrintTest extends FreeSpec {
+class ArrayUtilTest extends FreeSpec {
 
   "blah" in {
     val arr: Array[(Any, Any)] = Array(("hi", "there"), ("fun", "train"))
-    val res                    = ArrayPrettyPrint.weirdTypesToStrings(arr, 10)
+    val res                    = ArrayUtil.weirdTypesToStrings(arr, 10)
     assert(res sameElements Array(List("hi", "there"), List("fun", "train")))
   }
 
   "showTwoColumnString" in {
     val arr: Array[(Any, Any)] = Array(("word1", "word2"), ("hi", "there"), ("fun", "train"))
-    println(ArrayPrettyPrint.showTwoColumnString(arr, 10))
+    println(ArrayUtil.showTwoColumnString(arr, 10))
   }
 
   "showTwoColumnDate" in {
     val now = LocalDate.now()
     val arr: Array[(Any, Any)] =
       Array(("word1", "word2"), (Date.valueOf(now), Date.valueOf(now)), (Date.valueOf(now.plusDays(-1)), Date.valueOf(now)))
-    println(ArrayPrettyPrint.showTwoColumnString(arr, 10))
+    println(ArrayUtil.showTwoColumnString(arr, 10))
   }
 
   "dumbshowTwoColumnString" in {
     val arr: Array[(Any, Any)] = Array(("word1", "word2"), ("hi", "there"), ("fun", "train"))
     val rowEqual               = Array(true, false)
-    println(ArrayPrettyPrint.showTwoColumnStringColorCustomizable(arr, rowEqual))
+    println(ArrayUtil.showTwoColumnStringColorCustomizable(arr, rowEqual))
   }
 
 }
