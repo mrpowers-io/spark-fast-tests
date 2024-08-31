f42d3b3a31feca8642bac513109a713fff65f992
happytomatoe <2893931+happytomatoe@users.noreply.github.com>
2024-08-31 12:18:53 +0200

    WIP

--- src/main/scala/com/github/mrpowers/spark/fast/tests/ufansi/FansiExtensions.scala
+++ src/main/scala/com/github/mrpowers/spark/fast/tests/ufansi/FansiExtensions.scala
@@ -2,6 +2,9 @@ package com.github.mrpowers.spark.fast.tests.ufansi
 object FansiExtensions {
   implicit class StrOps(c: Seq[Str]) {
     def mkStr(start: Str, sep: Str, end: Str): Str =
-      start ++ c.reduce(_ ++ sep ++ _) ++ end
+      c.foldLeft(start)((leftAcc: Str, e: Str) => {
+        val separator = if (leftAcc == start) Str("") else sep
+        leftAcc ++ separator ++ e
+      }) ++ end
   }
 }
