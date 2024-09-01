267f9ab38dbcef9e0ce050032ba76fd876a739ac
happytomatoe <2893931+happytomatoe@users.noreply.github.com>
2024-09-01 07:51:33 +0200

    Change mkStr to use reduce

--- src/main/scala/com/github/mrpowers/spark/fast/tests/ufansi/FansiExtensions.scala
+++ src/main/scala/com/github/mrpowers/spark/fast/tests/ufansi/FansiExtensions.scala
@@ -2,9 +2,6 @@ package com.github.mrpowers.spark.fast.tests.ufansi
 object FansiExtensions {
   implicit class StrOps(c: Seq[Str]) {
     def mkStr(start: Str, sep: Str, end: Str): Str =
-      c.foldLeft(start)((leftAcc: Str, e: Str) => {
-        val separator = if (leftAcc == start) Str("") else sep
-        leftAcc ++ separator ++ e
-      }) ++ end
+      start ++ c.reduce(_ ++ sep ++ _) ++ end
   }
 }
