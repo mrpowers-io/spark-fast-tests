2ce453895ec5798a702f44591cb9a20a042f38ed
Tuan Pham <Tuan.Pham@wisetechglobal.com>
2024-09-18 21:18:40 +1000

    Benchmark assertApproximateDataFrameEquality with more data

--- benchmarks/src/main/scala/com/github/mrpowers/spark/fast/tests/DataFrameComparerBenchmark.scala
+++ benchmarks/src/main/scala/com/github/mrpowers/spark/fast/tests/DataFrameComparerBenchmark.scala
@@ -7,7 +7,7 @@ import org.openjdk.jmh.infra.Blackhole
 import java.util.concurrent.TimeUnit
 import scala.util.Try
 
-private class MyBenchmark extends DataFrameComparer {
+private class DataFrameComparerBenchmark extends DataFrameComparer {
   @Benchmark
   @BenchmarkMode(Array(Mode.AverageTime, Mode.SingleShotTime))
   @Fork(value = 2)
@@ -24,15 +24,13 @@ private class MyBenchmark extends DataFrameComparer {
     spark.sparkContext.setLogLevel("ERROR")
 
     import spark.implicits._
-    val ds1 = Seq(
-      ("1", "10/01/2019", 26.762499999999996),
-      ("1", "11/01/2019", 26.762499999999996)
-    ).toDF("col_B", "col_C", "col_A")
+    val sameData = Seq.fill(50)("1", "10/01/2019", 26.762499999999996)
+    val ds1Data = sameData ++ Seq.fill(50)("1", "11/01/2019", 26.76249999999991)
+    val ds2Data = sameData ++ Seq.fill(50)("3", "12/01/2019", 26.76249999999991)
 
-    val ds2 = Seq(
-      ("1", "10/01/2019", 26.762499999999946),
-      ("1", "11/01/2019", 26.76249999999991)
-    ).toDF("col_B", "col_C", "col_A")
+    val ds1 = ds1Data.toDF("col_B", "col_C", "col_A")
+    val ds2 = ds2Data.toDF("col_B", "col_C", "col_A")
+
     val result = Try(assertApproximateDataFrameEquality(ds1, ds2, precision = 0.0000001, orderedComparison = false))
 
     blackHole.consume(result)
