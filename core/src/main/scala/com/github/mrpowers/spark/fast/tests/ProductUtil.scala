92766971447e00089cc97cf9b03233ae7373fcb8
Tuan Pham <Tuan.Pham@wisetechglobal.com>
2024-10-02 08:04:18 +1000

    Make Row Diff not display class name

--- core/src/main/scala/com/github/mrpowers/spark/fast/tests/ProductUtil.scala
+++ core/src/main/scala/com/github/mrpowers/spark/fast/tests/ProductUtil.scala
@@ -26,9 +26,8 @@ object ProductUtil {
   ): String = {
 
     val runTimeClass                     = implicitly[ClassTag[T]].runtimeClass
-    val className                        = runTimeClass.getSimpleName
-    val border                           = if (runTimeClass == classOf[Row]) ("[", "]") else ("(", ")")
-    val prodToString: Seq[Any] => String = s => s.mkString(s"$className${border._1}", ",", border._2)
+    val (className, lBracket, rBracket)  = if (runTimeClass == classOf[Row]) ("", "[", "]") else (runTimeClass.getSimpleName, "(", ")")
+    val prodToString: Seq[Any] => String = s => s.mkString(s"$className$lBracket", ",", rBracket)
     val emptyProd                        = "MISSING"
 
     val sb = new StringBuilder
@@ -62,9 +61,9 @@ object ProductUtil {
                 case (actualRowField, expectedRowField, false) =>
                   (Red(actualRowField.toString), Green(expectedRowField.toString))
               }
-            val start = DarkGray(s"$className${border._1}")
+            val start = DarkGray(s"$className$lBracket")
             val sep   = DarkGray(",")
-            val end   = DarkGray(border._2)
+            val end   = DarkGray(rBracket)
             List(
               coloredDiff.map(_._1).mkStr(start, sep, end),
               coloredDiff.map(_._2).mkStr(start, sep, end)
