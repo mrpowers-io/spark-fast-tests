90ec1e1a49922dca7ff864a35fcc52a27c3c82f9
Tuan Pham <Tuan.Pham@wisetechglobal.com>
2024-12-10 09:00:11 +1100

    Add benchmark with join column

--- benchmarks/src/main/scala/com/github/mrpowers/spark/fast/tests/DatasetComparerBenchmark.scala
+++ benchmarks/src/main/scala/com/github/mrpowers/spark/fast/tests/DatasetComparerBenchmark.scala
@@ -37,6 +37,29 @@ private class DatasetComparerBenchmark extends DatasetComparer {
   @Warmup(iterations = 10)
   @Measurement(iterations = 10)
   @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  def assertLargeDatasetEqualityV2WithPrimaryKey(blackHole: Blackhole): Boolean = {
+    val spark = SparkSession
+      .builder()
+      .master("local")
+      .appName("spark session")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val ds1 = spark.range(0, 1000000, 1, 8)
+    val ds3 = ds1
+
+    val result = Try(assertLargeDatasetEqualityV2(ds1, ds3, primaryKeys = Seq("id")))
+
+    blackHole.consume(result)
+    result.isSuccess
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.SingleShotTime))
+  @Fork(value = 2)
+  @Warmup(iterations = 10)
+  @Measurement(iterations = 10)
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
   def assertLargeDatasetEquality(blackHole: Blackhole): Boolean = {
     val spark = SparkSession
       .builder()
